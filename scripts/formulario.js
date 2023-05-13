ef42ffbb4bcf9068ac046e27f299d79fbd9aa403
tomas0011 <tomasbenjamin117@gmail.com>
2023-05-13 15:50:22 -0300

    add form funcionality and validations

--- scripts/formulario.js
+++ scripts/formulario.js
@@ -1,16 +1,34 @@
 let formData = {
+  emailError: false,
   emailInput: '',
-  textareaInput: ''
-}
-
-const handlerOnSubmit = (e) => {
-  // console.log(formData)
+  textareaInput: '',
+  subjectInput: ''
 }
 
 const handlerOnChange = (e) => {
-  formData[e.target.id] = e.target.value
+  if (e.target.id === 'emailInput') {
+    const emailValidator = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+    const initialValue = formData.emailError;
+    formData.emailError = !emailValidator.test(e.target.value);
+    if (initialValue != formData.emailError) {
+      e.target.classList.toggle('inputError');
+    }
+  }
+  formData[e.target.id] = e.target.value;
+  let successButton = document.querySelector("#successButton");
+  let [base, props] = successButton.getAttribute('href').split('?');
+  let [subject, body] = props.split('&');
+
+  const setAttribute = (attribute, data, separator) => [attribute.split(separator)[0], data].join(separator);
+
+  base = setAttribute(base, formData.emailInput, ':');
+  subject = setAttribute(subject, formData.subjectInput, '=');
+  body = setAttribute(body, formData.textareaInput, '=');
+  props = [subject, body].join('&');
+  url = [base, props].join('?');
+  document.querySelector("#successButton").setAttribute('href', url);
 }
 
-document.querySelector("#emailInput").addEventListener('input', handlerOnChange)
-document.querySelector("#textareaInput").addEventListener('input', handlerOnChange)
-document.querySelector("#successButton").addEventListener('click', handlerOnSubmit)
+document.querySelector("#emailInput").addEventListener('input', handlerOnChange);
+document.querySelector("#subjectInput").addEventListener('input', handlerOnChange);
+document.querySelector("#textareaInput").addEventListener('input', handlerOnChange);
--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,16 @@
-// Boton para intercambiar modo claro y modo oscuro
-let switchColorsButton = document.querySelector("#switchColors")
-let switchColorsIcon = document.querySelector("#switchColorsIcon")
-
-
+let switchColorsButton = document.querySelector("#switchColors");
+let switchColorsIcon = document.querySelector("#switchColorsIcon");
+let body = document.querySelector("body");
 
 switchColorsButton.addEventListener('click', (e) => {
-  let paletaDeColores = document.querySelector(':root');
   if (e.target.textContent === 'Modo Oscuro') {
-    e.target.textContent = 'Modo Claro'
-    paletaDeColores.style.setProperty('--letras', '#d4f985');
-    paletaDeColores.style.setProperty('--fondo', '#9e8ac8');
-    paletaDeColores.style.setProperty('--contraste', '#000000');
+    e.target.textContent = 'Modo Claro';
   } else {
-    e.target.textContent = 'Modo Oscuro'
-    paletaDeColores.style.setProperty('--letras', 'orange');
-    paletaDeColores.style.setProperty('--fondo', '#25114A');
-    paletaDeColores.style.setProperty('--contraste', '#ffffff');
-  }
-  e.target.classList.toggle('btn-light')
-  e.target.classList.toggle('btn-dark')
-  switchColorsIcon.classList.toggle('mdi-weather-night')
-  switchColorsIcon.classList.toggle('mdi-weather-sunny')  
+    e.target.textContent = 'Modo Oscuro';
+  };
+  body.classList.toggle('darkMode');
+  e.target.classList.toggle('btn-light');
+  e.target.classList.toggle('btn-dark');
+  switchColorsIcon.classList.toggle('mdi-weather-night');
+  switchColorsIcon.classList.toggle('mdi-weather-sunny') ;
 })
